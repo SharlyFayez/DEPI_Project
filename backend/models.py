40843627a3d10b9095de40172b64a35e192db013
Sharly Fayez <131978254+SharlyFayez@users.noreply.github.com>
2026-05-23 08:20:32 +0200

    Delete backend directory

--- backend/models.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
-
-class TrafficData(Base):
-    __tablename__ = "traffic_data"
-
-    id = Column(Integer, primary_key=True, index=True)
-    count = Column(Integer)
-    time = Column(String)
